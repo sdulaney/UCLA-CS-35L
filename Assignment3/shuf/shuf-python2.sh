b5439530e755c0190b2c9d07da781e81616d6ba6
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-22 17:47:27 -0700

    Update files

--- Assignment3/shuf/shuf-python2.sh
+++ Assignment3/shuf/shuf-python2.sh
@@ -10,7 +10,7 @@ seq 100 > in
 ##### Input mode 1: STDIN
 
 # no FILE, 0 option arguments
-cat in | python3 shuf.py >out || fail=1
+cat in | python shuf.py >out || fail=1
 c=$(wc -l < out)
 test "$c" -eq 100 || { fail=1; echo "no FILE, 0 option arguments failed">&2 ; }
 
@@ -24,7 +24,7 @@ sort -n out > out1
 cmp in out1 > /dev/null || { fail=1; echo "not a permutation" 1>&2; }
 
 # FILE is -, 0 option arguments
-cat in | python3 shuf.py - >out || fail=1
+cat in | python shuf.py - >out || fail=1
 c=$(wc -l < out)
 test "$c" -eq 100 || { fail=1; echo "FILE is -, 0 option arguments failed">&2 \
 ; }
@@ -41,7 +41,7 @@ cmp in out1 > /dev/null || { fail=1; echo "not a permutation" 1>&2; }
 ##### Input mode 2: FILE
 
 # FILE is not -, 0 option arguments
-python3 shuf.py in >out || fail=1
+python shuf.py in >out || fail=1
 c=$(wc -l < out)
 test "$c" -eq 100 || { fail=1; echo "FILE is not -, 0 option arguments failed">&2 \
 ; }
@@ -56,68 +56,68 @@ sort -n out > out1
 cmp in out1 > /dev/null || { fail=1; echo "not a permutation" 1>&2; }
 
 # Test file does not exist fails
-python3 shuf.py in1 >out 2>out.2
+python shuf.py in1 >out 2>out.2
 test $? -eq 2 || fail=1
 
 # Test file missing read permissions fails
 touch in1
 chmod -r in1
-python3 shuf.py in1 >out 2>out.2
+python shuf.py in1 >out 2>out.2
 test $? -eq 2 || fail=1
 rm in1
 
 # Test multiple non-option arguments fails
-python3 shuf.py in in1 >out 2>out.2
+python shuf.py in in1 >out 2>out.2
 test $? -eq 2 || fail=1
 
 ##### Input mode 3: option -i (Act as if input came from a file containing the range of unsigned decimal integers lo...hi, one per line.)
 
 # Exercise shuf's -i option.
-python3 shuf.py -i 1-100 > out || fail=1
+python shuf.py -i 1-100 > out || fail=1
 cmp in out > /dev/null && { fail=1; echo "not random?" 1>&2; }
 sort -n out > out1
 cmp in out1 > /dev/null || { fail=1; echo "not a permutation" 1>&2; }
 
 # Test invalid range for -i
 for ARG in '1' 'A' '1-' '1-A' '3-1'; do
-    { python3 shuf.py -i$ARG > out 2> out.2 || test $? -ne 2; } &&
+    { python shuf.py -i$ARG > out 2> out.2 || test $? -ne 2; } &&
     { fail=1; echo "shuf did not detect erroneous -i$ARG usage.">&2 ; }
 done
 
 # -i cannot be used with any non-option arguments
-python3 shuf.py -i 1-100 - > out 2> out.2
+python shuf.py -i 1-100 - > out 2> out.2
 test $? -eq 2 || fail=1
-python3 shuf.py -i 1-100 in > out 2> out.2
+python shuf.py -i 1-100 in > out 2> out.2
 test $? -eq 2 || fail=1
 touch in2
-python3 shuf.py -i 1-100 in in2 > out 2> out.2
+python shuf.py -i 1-100 in in2 > out 2> out.2
 test $? -eq 2 || fail=1
 rm in2
 
 ##### Option --head-count (-n)
 
 # Exercise shuf's -n option.
-python3 shuf.py -i 1-100 -n 9 > out || fail=1
+python shuf.py -i 1-100 -n 9 > out || fail=1
 c=$(wc -l < out)
 test "$c" -eq 9 || { fail=1; echo "Option -n failed">&2 ; }
 
 # Test invalid arguments for option -n
-python3 shuf.py in -n A >out 2>out.2
+python shuf.py in -n A >out 2>out.2
 test $? -eq 2 || fail=1
-python3 shuf.py in -n -1 >out 2>out.2
+python shuf.py in -n -1 >out 2>out.2
 test $? -eq 2 || fail=1
 
 ##### Option --repeat (-r)
 
 # Exercise shuf's -r option.
-python3 shuf.py -i 1-10 -n 100 -r > out || fail=1
+python shuf.py -i 1-10 -n 100 -r > out || fail=1
 c=$(wc -l < out)
 test "$c" -eq 100 || { fail=1; echo "Option -r failed">&2 ; }
 
 ##### Option --help
 
 # Exercise shuf's --help option.
-python3 shuf.py --help > out || fail=1
+python shuf.py --help > out || fail=1
 cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 
 
@@ -140,7 +140,7 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 
 
 # Your program should support a single non-option argument other than "-" (which specifies the input file name).
-#python3 shuf.py in >out || fail=1
+#python shuf.py in >out || fail=1
 
 # Fail if the input is the same as the output.
 # This is a probabilistic test :-)
@@ -158,20 +158,20 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 # TODO: option -n with missing/invalid mandatory argument
 
 # Exercise shuf's --input-range option.
-#python3 shuf.py --input-range=1-100 > out || fail=1
+#python shuf.py --input-range=1-100 > out || fail=1
 #compare in out > /dev/null && { fail=1; echo "not random?" 1>&2; }
 #sort -n out > out1
 #compare in out1 || { fail=1; echo "not a permutation" 1>&2; }
 
 
 # Exercise shuf's --head-count option.
-#python3 shuf.py -i 1-100 --head-count=9 > out || fail=1
+#python shuf.py -i 1-100 --head-count=9 > out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 9 || { fail=1; echo "Option --head-count failed">&2 ; }
 
 
 # Exercise shuf's --repeat option.
-#python3 shuf.py -i 1-10 -n 100 --repeat  > out || fail=1
+#python shuf.py -i 1-10 -n 100 --repeat  > out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 100 || { fail=1; echo "Option --repeat failed">&2 ; }
 
@@ -180,75 +180,75 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 
 
 # Your program should support zero non-option arguments (means read from standard input) - 1 options
-#cat in | python3 shuf.py -i 0-9 >out || fail=1
+#cat in | python shuf.py -i 0-9 >out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 10 || { fail=1; echo "Zero non-option arguments and one option arguments failed (-i should override no FILE and not read from STDIN)">&2 ; }
 
 # Your program should support zero non-option arguments (means read from standard input) - 2 options
-#cat in | python3 shuf.py -r -n 1000 >out || fail=1
+#cat in | python shuf.py -r -n 1000 >out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 1000 || { fail=1; echo "Zero non-option arguments and two option arguments failed">&2 ; }
 
 # Your program should support zero non-option arguments (means read from standard input) - 3 options
-#cat in | python3 shuf.py -r -n 1000 -i 0-9 >out || fail=1
+#cat in | python shuf.py -r -n 1000 -i 0-9 >out || fail=1
 #c=$(sort -u < out)
 #test "$c" -eq 10 || { fail=1; echo "Zero non-option arguments and three  option arguments failed (-i should override no FILE and not read from STDIN)">&2 ; }
 
 # Your program should support a single non-option argument "-" (means read from standard input) - 0 options
-#cat in | python3 shuf.py - >out || fail=1
+#cat in | python shuf.py - >out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 100 || { fail=1; echo "Single non-option argument "-" and zero option arguments failed">&2 ; }
 
 # Your program should support a single non-option argument "-" (means read from standard input) - 1 options (option -i and FILE equal to - should crash the program)
-#cat in | python3 shuf.py - -i 0-9 >out
+#cat in | python shuf.py - -i 0-9 >out
 #test $? -eq 2 || fail=1
 
 # Your program should support a single non-option argument "-" (means read from standard input) - 2 options
-#cat in | python3 shuf.py - -r -n 1000 >out || fail=1
+#cat in | python shuf.py - -r -n 1000 >out || fail=1
 #c=$(wc -l < out)
 #test "$c" -eq 1000 || { fail=1; echo "Single non-option argument "-" and two option arguments failed">&2 ; }
 
 # Your program should support a single non-option argument "-" (means read from standard input) - 3 options (option -i and FILE equal to - should crash the program)
-#cat in | python3 shuf.py - -r -n 1000 -i 0-9 >out
+#cat in | python shuf.py - -r -n 1000 -i 0-9 >out
 #test $? -eq 2 || fail=1
 
 # Invalid arguments
-#python3 shuf.py -r /dev/null
+#python shuf.py -r /dev/null
 #test $? -eq 2 || fail=1
 
 # Invalid arguments: with a single redundant operand with --input-range
-#python3 shuf.py -i0-0 1
+#python shuf.py -i0-0 1
 #test $? -eq 2 || fail=1
 
 # Avoid infloop.
 # "seq 1860" produces 8193 (8K + 1) bytes of output.
-#seq 1860 | python3 shuf.py > /dev/null || fail=1
+#seq 1860 | python shuf.py > /dev/null || fail=1
 
 # Ensure shuf -n operates efficiently for small n. Before coreutils-8.13
 # this would try to allocate $SIZE_MAX * sizeof(size_t)
-#timeout 10 python3 shuf.py -i1-$SIZE_MAX -n2 >/dev/null ||
+#timeout 10 python shuf.py -i1-$SIZE_MAX -n2 >/dev/null ||
  #   { fail=1; echo "couldn't get a small subset" >&2; }
 
 # Ensure shuf -n0 doesn't read any input or open specified files
 #touch unreadable
 #chmod 0 unreadable
 #if ! test -r unreadable; then
-#  python3 shuf.py -n0 unreadable || fail=1
-#  { python3 shuf.py -n1 unreadable || test $? -ne 2; } && fail=1
+#  python shuf.py -n0 unreadable || fail=1
+#  { python shuf.py -n1 unreadable || test $? -ne 2; } && fail=1
 #fi
 
 # Multiple -n is accepted, should use the smallest value
-#python3 shuf.py -n10 -i0-9 -n3 -n20 > exp
+#python shuf.py -n10 -i0-9 -n3 -n20 > exp
 #c=$(wc -l < exp)
 #test "$c" -eq 3 || { fail=1; echo "Multiple -n failed">&2 ; }
 
 # Test error conditions
 
 # Test invalid value for -n
-#{ python3 shuf.py -nA || test $? -ne 2; } &&
+#{ python shuf.py -nA || test $? -ne 2; } &&
 #  { fail=1; echo "shuf did not detect erroneous -n usage.">&2 ; }
 # Test multiple -i
-#{ python3 shuf.py -i0-9 -n10 -i8-90 || test $? -ne 2; } &&
+#{ python shuf.py -i0-9 -n10 -i8-90 || test $? -ne 2; } &&
 #  { fail=1; echo "shuf did not detect multiple -i usage.">&2 ; }
 
 # Test --repeat option
@@ -256,7 +256,7 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 
 
 # --repeat can output more values than the input range
-#python3 shuf.py --rep -i0-9 -n1000 > exp
+#python shuf.py --rep -i0-9 -n1000 > exp
 #c=$(wc -l < exp)
 #test "$c" -eq 1000 || { fail=1; echo "--repeat with --count failed">&2 ; }
 
@@ -268,25 +268,25 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 #    { fail=1; echo "--repeat produced bad output">&2 ; }
 
 # check --repeat with non-zero low value
-#python3 shuf.py --rep -i222-233 -n2000 > exp
+#python shuf.py --rep -i222-233 -n2000 > exp
 #c=$(sort -nu exp | paste -s -d ' ')
 #test "$c" = "222 223 224 225 226 227 228 229 230 231 232 233" ||
 # { fail=1; echo "--repeat produced bad output with non-zero low">&2 ; }
 
 # --repeat,-i,count=0 should not fail and produce no output
-#python3 shuf.py --rep -i0-9 -n0 > exp
+#python shuf.py --rep -i0-9 -n0 > exp
 # file size should be zero (no output from shuf)
 #test \! -s exp ||
 #    { fail=1; echo "--repeat,-i0-9,-n0 produced bad output">&2 ; }
 
 # --repeat with STDIN, without count, should repeat indefinitely
-#printf "A\nB\nC\nD\nE\n" | python3 shuf.py --rep | head -n 1000 > exp
+#printf "A\nB\nC\nD\nE\n" | python shuf.py --rep | head -n 1000 > exp
 #c=$(wc -l < exp)
 #test "$c" -eq 1000 ||
 #  { fail=1; echo "--repeat,STDIN does not repeat indefinitely">&2 ; }
 
 # --repeat with STDIN,count - can return more values than input lines
-#printf "A\nB\nC\nD\nE\n" | python3 shuf.py --rep -n2000 > exp
+#printf "A\nB\nC\nD\nE\n" | python shuf.py --rep -n2000 > exp
 #c=$(wc -l < exp)
 #test "$c" -eq 2000 ||
 #    { fail=1; echo "--repeat,STDIN,count failed">&2 ; }
@@ -299,14 +299,14 @@ cmp out shuf--help.txt || { fail=1; echo "Option --help failed">&2 ; }
 #  { fail=1; echo "--repeat,STDIN,count produced bad output">&2 ; }
 
 # --repeat,stdin,count=0 should not fail and produce no output
-#printf "A\nB\nC\nD\nE\n" | python3 shuf.py --rep -n0 > exp
+#printf "A\nB\nC\nD\nE\n" | python shuf.py --rep -n0 > exp
 # file size should be zero (no output from shuf)
 #test \! -s exp ||
 #    { fail=1; echo "--repeat,STDIN,-n0 produced bad output">&2 ; }
 
 # shuf 8.25 mishandles input if stdin is closed, due to glibc bug#15589.
 # See coreutils bug#25029.
-#python3 shuf.py /dev/null <&- >out || fail=1
+#python shuf.py /dev/null <&- >out || fail=1
 #compare /dev/null out || fail=1
 
 #exit $fail
--- Assignment3/shuf/shuf.py
+++ Assignment3/shuf/shuf.py
@@ -8,10 +8,13 @@ import random, sys
 from optparse import OptionParser
 
 class shufline:
-    def __init__(self, filename):
-        f = open(filename, 'r')
-        self.lines = f.readlines()
-        f.close()
+    def __init__(self, filename=None, input_lines=None):
+        if filename is not None:
+            f = open(filename, 'r')
+            self.lines = f.read().splitlines()
+            f.close()
+        elif input_lines is not None:
+            self.lines = input_lines[:]
 
     def get_lines(self, head_lines=None, repeat=False):
         if repeat:
@@ -36,42 +39,105 @@ class shufline:
                     return shuffled_lines[0:head_lines]
 
 def main():
-#    version_msg = "%prog 2.0"
-#    usage_msg = """%prog [OPTION]... FILE
-#
-#Output randomly selected lines from FILE."""
+    version_msg = "%prog 1.0"
+    usage_msg = """
+%prog [OPTION]... [FILE]
+%prog -i LO-HI [OPTION]...
 
-    generator = shufline("in")
-    output = generator.get_lines()
-    print("".join(output), end="")
+Generate random permutations/shuffle lines of text."""
     
-#    parser = OptionParser(version=version_msg,
-#                          usage=usage_msg)
-#    parser.add_option("-n", "--numlines",
-#                      action="store", dest="numlines", default=1,
-#                      help="output NUMLINES lines (default 1)")
-#    options, args = parser.parse_args(sys.argv[1:])
+    parser = OptionParser(version=version_msg,
+                          usage=usage_msg)
+    parser.add_option("-i", "--input-range",
+                      action="store", dest="input_range", default=None,
+                      help="Act as if input came from a file containing the range of unsigned dec\
+imal integers LO...HI, one per line.\n")
+    parser.add_option("-n", "--head-count",
+                      action="store", dest="head_count", default=None,
+                      help="Output at most COUNT lines. By default, all input lines are output.\n")
+    parser.add_option("-r", "--repeat",
+                      action="store_true", dest="repeat", default=False,
+                      help="Repeat output values, that is, select with replacement. With this option the output is not a permutation of the input; instead, each output line is randomly chosen from all the inputs. This option is typically combined with --head-count; if --head-count is not given, shuf.py repeats indefinitely.\n")
+    options, args = parser.parse_args(sys.argv[1:])
 
-#    try:
-#        numlines = int(options.numlines)
-#    except:
-#        parser.error("invalid NUMLINES: {0}".
-#                     format(options.numlines))
-#    if numlines < 0:
-#        parser.error("negative count: {0}".
-#                     format(numlines))
-#    if len(args) != 1:
-#        parser.error("wrong number of operands")
-#    input_file = args[0]
+    input_lines = None
+    input_file = None
+    numlines = None
 
-#    try:
-#        generator = randline(input_file)
-#        for index in range(numlines):
-#            sys.stdout.write(generator.chooseline())
-#    except IOError as err:
-#        errno, strerror = err.args
-#        parser.error("I/O error({0}): {1}".
-#                     format(errno, strerror))
+    # option --input-range (-i)
+    if options.input_range is not None:
+        if len(args) != 0:
+            parser.error("extra operand \'{0}\'".format(args[0]))
+        lo_hi = options.input_range.split("-", 1)
+        if len(lo_hi) == 1:
+            parser.error("invalid input range: \'{0}\'".format(lo_hi[0]))
+        try:
+            lo = int(lo_hi[0])
+        except:
+            parser.error("invalid input range: \'{0}\'".format(lo_hi[0]))
+        try:
+            hi = int(lo_hi[1])
+        except:
+            parser.error("invalid input range: \'{0}\'".format(lo_hi[1]))
+        if lo > hi + 1:
+            parser.error("invalid input range: \'{0}\'".format(options.input_range))
+        
+        input_lines = list(range(lo, hi + 1))
+    else:
+        if len(args) > 1:
+            parser.error("wrong number of operands")
+        elif len(args) == 0:
+            try:
+                input_lines = sys.stdin.read().splitlines()
+            except IOError as err:
+                errno, strerror = err.args
+                parser.error("I/O error({0}): {1}".
+                         format(errno, strerror))
+        elif len(args) == 1:
+            if args[0] == "-":
+                try:
+                    input_lines = sys.stdin.read().splitlines()
+                except IOError as err:
+                    errno, strerror = err.args
+                    parser.error("I/O error({0}): {1}".
+                         format(errno, strerror))
+            else:
+                input_file = args[0]
+                try:
+                    temp_f = open(input_file, 'r')
+                    temp_f.close()
+                except:
+                    parser.error("Can not open file {0}.".format(input_file))
+
+    # option --head-count (-n)
+    if options.head_count is not None:
+        try:
+            numlines = int(options.head_count)
+        except:
+            parser.error("invalid line count: \'{0}\'".
+                     format(options.head_count))
+        if numlines < 0:
+            parser.error("invalid line count: \'{0}\'".
+                     format(options.head_count))
+
+    generator = shufline(filename=input_file, input_lines=input_lines)
+
+    # option --repeat (-r)
+    if options.repeat == True:
+        if len(generator.lines) == 0:
+            parser.error("no lines to repeat")
+        if numlines == None:
+            while True:
+                sys.stdout.write(str(generator.get_lines(head_lines=numlines, repeat=options.repeat)) + "\n")
+    
+    try:
+        output = generator.get_lines(head_lines=numlines, repeat=options.repeat)
+        for line in output:
+            sys.stdout.write(str(line) + "\n")
+    except IOError as err:
+        errno, strerror = err.args
+        parser.error("I/O error({0}): {1}".
+                     format(errno, strerror))
 
 if __name__ == "__main__":
     main()
