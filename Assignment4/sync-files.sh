a11231842e174e0c9b367d137e38fba11e24f41d
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-29 15:29:06 -0700

    Update files

--- Assignment4/sync-files.sh
+++ Assignment4/sync-files.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 cp /Users/stewart/Dropbox/@Education/Archive/UCLA/2019-20/Fall\ 2019/CS\ 35L\ -\ Software\ Construction\ Laboratory/Assignment4/lab4.txt .
+scp stewart@lnxsrv07.seas.ucla.edu:/u/cs/ugrad/stewart/Assignment4/lab4.diff .
