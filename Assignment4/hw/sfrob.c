689b0a1297f9eaf068ea11f3e40f7110fffe06bc
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-30 22:39:44 -0700

    Update files

---
+++ Assignment4/hw/sfrob.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Returns a value that is negative, zero, or positive depending on whether a is less than, equal to, or greater than b. 
+int frobcmp(char const * a, char const * b)
+{
+  printf("Hello, World!");
+  return 0;
+}
+
+int main()
+{
+  frobcmp("a", "b");
+  
+  return 0;
+}
