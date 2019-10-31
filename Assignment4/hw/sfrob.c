79c5d47f132c6ec815ac2a3dc7441279a509c067
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-31 06:20:36 -0700

    Update files

--- Assignment4/hw/sfrob.c
+++ Assignment4/hw/sfrob.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 // Returns a value that is negative, zero, or positive depending on whether a is less than, equal to, or greater than b. 
@@ -37,6 +38,15 @@ int frobcmp(char const * a, char const * b)
   }
 }
 
+// Wrapper of frobcmp using void pointers to pass to qsort.
+int cmp(const void* x, const void* y)
+{
+    // Cast from void** to char** and then dereference.
+    const char* a = *((const char**) x);
+    const char* b = *((const char**) y);
+    return frobcmp(a, b);
+}
+
 int main()
 {
   // Unit tests for frobcmp 
@@ -58,10 +68,69 @@ int main()
   assert(frobcmp("{_CIA*\031 ", "{_CIA\030\031 ") < 0);
   // null byte in 2nd arg contributes to comparison
   assert(frobcmp("{_CIA\030\031 ", "{_CIA*\031 ") > 0);
-  
-  
 
+  char** wordlist = NULL;
+  int wordlist_len = 0;
+  char* word = NULL;
+  int word_len = 0;
   
+  // Read from STDIN byte by byte until getchar returns EOF (indicating failure).
+  char ch;
+  while (ch = getchar())
+  {
+      if (ch != EOF)
+      {
+	  if (word == NULL)
+	  {
+	      word = (char*) malloc(sizeof(char));
+	  }
+	  else
+	  {
+	      word = (char*) realloc(word, (word_len + 1) * sizeof(char));
+	  }
+	  word[word_len] = ch;
+	  word_len++;
+      }
+      if (ch == ' ' || (ch == EOF && word_len > 0))
+      {
+	  if (ch == EOF && word_len > 0)
+	  {
+	      word = (char*) realloc(word, (word_len + 1) * sizeof(char));
+	      word[word_len] = ' ';
+	      word_len++;
+	  }
+	  if (wordlist == NULL)
+	  {
+	      wordlist = (char**) malloc(sizeof(char*));
+	  }
+	  else
+	  {
+	      wordlist = (char**) realloc(wordlist, (wordlist_len + 1) * sizeof(char*));
+	  }
+          wordlist[wordlist_len] = word;
+	  wordlist_len++;
+	  word = NULL;
+	  word_len = 0;
+      }
+      if (ch == EOF)
+      {
+	  break;
+      }
+  }
+
+  qsort(wordlist, wordlist_len, sizeof(char*), cmp);
+
+  for (int i = 0; i < wordlist_len; i++)
+  {
+      for (int j = 0; wordlist[i][j] != ' '; j++)
+      {
+	  putchar(wordlist[i][j]);
+      }
+      putchar(' ');
+      free(wordlist[i]);
+  }
+
+  free(wordlist);
  
   
   return 0;
--- Assignment4/hw/test-sfrob.sh
+++ Assignment4/hw/test-sfrob.sh
@@ -3,7 +3,7 @@
 test=1
 printf '*~BO *{_CIA *hXE]D *LER #@_GZY #E\\OX #^BO #FKPS #NEM\4' | ./sfrob | od -ta > out
 test $? -eq 0 || echo "Test $test: wrong exit code"
-cat '0000000   *   h   X   E   ]   D  sp   *   {   _   C   I   A  sp   *   ~
+echo '0000000   *   h   X   E   ]   D  sp   *   {   _   C   I   A  sp   *   ~
 0000020   B   O  sp   *   L   E   R  sp   #   N   E   M eot  sp   #   @
 0000040   _   G   Z   Y  sp   #   F   K   P   S  sp   #   E   \   O   X
 0000060  sp   #   ^   B   O  sp
