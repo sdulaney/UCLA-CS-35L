950c732da43062fc3c9babb9d04dcb663459215a
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-31 00:47:41 -0700

    Update files

--- Assignment4/hw/sfrob.c
+++ Assignment4/hw/sfrob.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
+#include <assert.h>
 
 // Returns a value that is negative, zero, or positive depending on whether a is less than, equal to, or greater than b. 
 int frobcmp(char const * a, char const * b)
 {
-  printf("Hello, World!");
-  return 0;
+  while (*a != ' ' && *b != ' ')
+  {
+    if (*a != *b)
+    {
+	char a_unfrob = *a ^ 0b00101010;
+	char b_unfrob = *b ^ 0b00101010;
+	return a_unfrob - b_unfrob;
+    }
+    a++;
+    b++;
+  }
+
+  if (*a == ' ' && *b == ' ')
+  {
+      return 0;
+  }
+  if (*a == ' ')
+  {
+      return -1;
+  }
+  if (*b == ' ')
+  {
+      return 1;
+  }
 }
 
 int main()
 {
-  frobcmp("a", "b");
+  // a > b
+  assert(frobcmp("*{_CIA\030\031 ", "*`_GZY\v ") > 0);
+  // a < b
+  assert(frobcmp("*k_CIA\030\031 ", "*`_GZY\v ") < 0);
+  // a = b
+  assert(frobcmp("*{_CIA\030\031 ", "*{_CIA\030\031 ") == 0);
+  // 1st arg is space byte: a < b
+  assert(frobcmp(" ", "*`_GZY\v ") < 0);
+  // 2nd arg is space byte: a > b
+  assert(frobcmp("*{_CIA\030\031 ", " ") > 0);
+  // 1st arg is prefix of 2nd: a < b
+  assert(frobcmp("*{_CIA ", "*{_CIA\030\031 ") < 0);
+  // 2nd arg is prefix of 1st: a > b
+  assert(frobcmp("*{_CIA\030\031 ", "*{_CIA ") > 0);
+  // null byte in 1st arg contributes to comparison
+  assert(frobcmp("{_CIA*\031 ", "{_CIA\030\031 ") < 0);
+  // null byte in 2nd arg contributes to comparison
+  assert(frobcmp("{_CIA\030\031 ", "{_CIA*\031 ") > 0);
+  
+  
+
+  
+ 
   
   return 0;
 }
