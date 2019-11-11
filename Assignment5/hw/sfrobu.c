dd89e23cbf9b979a9d543f9c7571cd32f5582845
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-11-11 15:18:41 -0800

    Update files

---
+++ Assignment5/hw/sfrobu.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// Returns a value that is negative, zero, or positive depending on whether a is less than, equal to, or greater than b. 
+int frobcmp(char const * a, char const * b)
+{
+  while (*a != ' ' && *b != ' ')
+  {
+    if (*a != *b)
+    {
+	char a_unfrob = *a ^ 0b00101010;
+	char b_unfrob = *b ^ 0b00101010;
+	if (a_unfrob > b_unfrob)
+	{
+	    return 1;
+	}
+	else if (a_unfrob < b_unfrob)
+	{
+	    return -1;
+	}
+    }
+    a++;
+    b++;
+  }
+
+  if (*a == ' ' && *b == ' ')
+  {
+      return 0;
+  }
+  if (*a == ' ')
+  {
+      return -1;
+  }
+  if (*b == ' ')
+  {
+      return 1;
+  }
+}
+
+// Wrapper of frobcmp using void pointers to pass to qsort.
+int cmp(const void* x, const void* y)
+{
+    // Cast from void** to char** and then dereference.
+    const char* a = *((const char**) x);
+    const char* b = *((const char**) y);
+    return frobcmp(a, b);
+}
+
+void free_memory(char** wordlist, int wordlist_len)
+{
+    for (int i = 0; i < wordlist_len; i++)
+    {
+	free(wordlist[i]);
+    }
+    free(wordlist);
+}
+
+void check_output_error(int ret_code, char** wordlist, int wordlist_len)
+{
+    if (ret_code == EOF)
+    {
+	if (ferror(stdout)) 
+        {
+	    free_memory(wordlist, wordlist_len);
+	    fprintf(stderr, "Output error.\n");
+	    exit(1);
+        }
+    }
+}
+
+void check_mem_alloc_error(void* ptr, char** wordlist, int wordlist_len)
+{
+    if (ptr == NULL)
+    {
+	free_memory(wordlist, wordlist_len);
+        fprintf(stderr, "Memory allocation error.\n");
+        exit(1);
+    }
+}
+
+
+int main()
+{
+  // Unit tests for frobcmp 
+  // a > b
+  assert(frobcmp("*{_CIA\030\031 ", "*`_GZY\v ") > 0);
+  // a < b
+  assert(frobcmp("*k_CIA\030\031 ", "*`_GZY\v ") < 0);
+  // a = b
+  assert(frobcmp("*{_CIA\030\031 ", "*{_CIA\030\031 ") == 0);
+  // 1st arg is space byte: a < b
+  assert(frobcmp(" ", "*`_GZY\v ") < 0);
+  // 2nd arg is space byte: a > b
+  assert(frobcmp("*{_CIA\030\031 ", " ") > 0);
+  // 1st arg is prefix of 2nd: a < b
+  assert(frobcmp("*{_CIA ", "*{_CIA\030\031 ") < 0);
+  // 2nd arg is prefix of 1st: a > b
+  assert(frobcmp("*{_CIA\030\031 ", "*{_CIA ") > 0);
+  // null byte in 1st arg contributes to comparison
+  assert(frobcmp("{_CIA*\031 ", "{_CIA\030\031 ") < 0);
+  // null byte in 2nd arg contributes to comparison
+  assert(frobcmp("{_CIA\030\031 ", "{_CIA*\031 ") > 0);
+
+  char** wordlist = NULL;
+  int wordlist_len = 0;
+  char* word = NULL;
+  int word_len = 0;
+  
+  // Read from STDIN byte by byte.
+  int ch;
+  while (1)
+  {
+      ch = getchar();
+
+      // If getchar failed due to an error besides end-of-file condition
+      if (ferror(stdin))
+      {
+	  free_memory(wordlist, wordlist_len);
+	  fprintf(stderr, "Input error.\n");
+	  exit(1);
+      }
+      
+      // If getchar failed due to end-of-file condition
+      if (feof(stdin))
+      {
+	  break;
+      }
+
+      if (word == NULL)
+      {
+	  word = (char*) malloc(sizeof(char));
+	  check_mem_alloc_error(word, wordlist, wordlist_len);
+      }
+      else
+      {
+	  word = (char*) realloc(word, (word_len + 1) * sizeof(char));
+	  check_mem_alloc_error(word, wordlist, wordlist_len);
+      }
+      word[word_len] = ch;
+      word_len++;
+
+      if (ch == ' ')
+      {
+	  if (wordlist == NULL)
+	  {
+	      wordlist = (char**) malloc(sizeof(char*));
+	      check_mem_alloc_error(wordlist, wordlist, wordlist_len);
+	  }
+	  else
+	  {
+	      wordlist = (char**) realloc(wordlist, (wordlist_len + 1) * sizeof(char*));
+	      check_mem_alloc_error(wordlist, wordlist, wordlist_len);
+	  }
+          wordlist[wordlist_len] = word;
+	  wordlist_len++;
+	  word = NULL;
+	  word_len = 0;
+      }
+  }
+
+  if (word_len > 0)
+  {
+      word = (char*) realloc(word, (word_len + 1) * sizeof(char));
+      check_mem_alloc_error(word, wordlist, wordlist_len);
+      word[word_len] = ' ';
+      word_len++;
+      if (wordlist == NULL)
+      {
+	  wordlist = (char**) malloc(sizeof(char*));
+	  check_mem_alloc_error(wordlist, wordlist, wordlist_len);
+      }
+      else
+      {
+	  wordlist = (char**) realloc(wordlist, (wordlist_len + 1) * sizeof(char*));
+	  check_mem_alloc_error(wordlist, wordlist, wordlist_len);
+      }
+      wordlist[wordlist_len] = word;
+      wordlist_len++;
+      word = NULL;
+      word_len = 0;
+  }
+
+  qsort(wordlist, wordlist_len, sizeof(char*), cmp);
+
+  for (int i = 0; i < wordlist_len; i++)
+  {
+      int putchar_ret_code = 0;
+      for (int j = 0; wordlist[i][j] != ' '; j++)
+      {
+	  putchar_ret_code = putchar(wordlist[i][j]);
+	  check_output_error(putchar_ret_code, wordlist, wordlist_len);
+      }
+      putchar_ret_code = putchar(' ');
+      check_output_error(putchar_ret_code, wordlist, wordlist_len);
+  }
+
+  free_memory(wordlist, wordlist_len); 
+  
+  return 0;
+}
---
+++ Assignment5/hw/test-sfrobu.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+option=$1
+
+# Using default C version per Piazza: https://piazza.com/class/k0zogkkf73r5dj?cid=268
+# __STDC_VERSION__ is 201710 (c17, a bugfix version of c11)
+gcc sfrob.c -o sfrob
+
+##### Test 1: Test case given in spec.
+test=1
+printf '*~BO *{_CIA *hXE]D *LER #@_GZY #E\\OX #^BO #FKPS #NEM\4' | ./sfrob | od -ta > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+echo '0000000   *   h   X   E   ]   D  sp   *   {   _   C   I   A  sp   *   ~
+0000020   B   O  sp   *   L   E   R  sp   #   N   E   M eot  sp   #   @
+0000040   _   G   Z   Y  sp   #   F   K   P   S  sp   #   E   \   O   X
+0000060  sp   #   ^   B   O  sp
+0000066' > expected_output
+cmp expected_output out > /dev/null || echo "Test $test: wrong STDOUT"
+
+##### Test 2: Expect the same output as Test 1 when there is a trailing space.
+test=2
+printf '*~BO *{_CIA *hXE]D *LER #@_GZY #E\\OX #^BO #FKPS #NEM\4 ' | ./sfrob | od -ta > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+cmp expected_output out > /dev/null || echo "Test $test: wrong STDOUT"
+
+##### Test 3: Test on empty file.
+test=3
+touch empty
+cat empty | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+cmp empty out > /dev/null || echo "Test $test: wrong STDOUT"
+
+##### Test 4: Test on relatively large file (rt1.jar - /usr/local/cs/jdk1.8.0_31/jre/lib/rt.jar).
+test=4
+cat rt1.jar | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+# out should be the same size or 1 byte larger than rt1.jar (when append trailing space)
+rt1_size=$(stat -c%s rt1.jar)
+out_size=$(stat -c%s out)
+size_difference=$(($out_size-$rt1_size))
+test $size_difference -eq 0 || test $size_difference -eq 1 || echo "Test $test: wrong STDOUT"
+
+##### Test 5: Test on relatively large file (rt2.jar - /usr/local/cs/jdk1.8.0_45/jre/lib/rt.jar).
+test=5
+cat rt2.jar | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+# out should be the same size or 1 byte larger than rt2.jar (when append trailing space)
+rt2_size=$(stat -c%s rt2.jar)
+out_size=$(stat -c%s out)
+size_difference=$(($out_size-$rt2_size))
+test $size_difference -eq 0 || test $size_difference -eq 1 || echo "Test $test: wrong STDOUT"
+
+##### Test 6: Test input error (when STDIN is closed).
+test=6
+./sfrob <&- > out.1 2> out.2
+test $? -eq 1 || echo "Test $test: wrong exit code"
+cmp -s out.2 /dev/null && echo "Test $test: STDERR empty"
+cmp out.1 /dev/null || echo "Test $test: STDOUT not empty"
+
+# Note: could not simulate this error by closing STDOUT.
+##### Test 7: Test output error (when STDOUT is closed).
+#test=7
+#printf '*~BO *{_CIA *hXE]D *LER #@_GZY #E\\OX #^BO #FKPS #NEM\4' | ./sfrob >&- > out.1 2> out.2
+#test $? -eq 1 || echo "Test $test: wrong exit code"
+#cmp -s out.2 /dev/null && echo "Test $test: STDERR empty"
+#cmp out.1 /dev/null || echo "Test $test: STDOUT not empty"
+
+##### Test 8: consecutive spaces should be treated as blank lines.
+test=8
+printf '*~BO *{_CIA *hXE]D *LER #@_GZY     #E\\OX #^BO #FKPS #NEM\4' | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+printf '    *hXE]D *{_CIA *~BO *LER #NEM\4 #@_GZY #FKPS #E\OX #^BO ' > expected_output
+cmp expected_output out > /dev/null || echo "Test $test: wrong STDOUT"
+
+##### Test 9: only spaces in input.
+test=9
+printf '     ' | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+printf '     ' > expected_output
+cmp expected_output out > /dev/null || echo "Test $test: wrong STDOUT"
+
+# Note: STDOUT not tested.
+##### Test 10: your program should work on the file /proc/self/status, a "file" that is constantly mutating.
+test=10
+cat /proc/self/status | ./sfrob > out
+test $? -eq 0 || echo "Test $test: wrong exit code"
+#printf '     ' > expected_output
+#cmp expected_output out > /dev/null || echo "Test $test: wrong STDOUT"
+
+# Note: did not fully test this.
+##### Test 11: test memory allocation error by throwing a ton of bytes into STDIN.
+#test=11
+#cat /dev/urandom | head -c 30000000000 | ./sfrob > out.1 2> out.2
+#test $? -eq 1 || echo "Test $test: wrong exit code"
+#cmp -s out.2 /dev/null && echo "Test $test: STDERR empty"
+#cmp out.1 /dev/null || echo "Test $test: STDOUT not empty"
